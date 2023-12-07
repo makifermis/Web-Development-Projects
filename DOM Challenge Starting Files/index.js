48d48b3b04e6058333906a25586c0281cdfa8ef8
Mehmet <makifermis@gmail.com>
2023-12-07 13:56:43 +0100

    intro DOM

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,3 @@
+document.querySelector("ul > li:nth-of-type(3)").innerHTML = "Mehmet"
+document.querySelector("a").style.color = "red"
+document.querySelector("button").style.backgroundColor = "yellow"
