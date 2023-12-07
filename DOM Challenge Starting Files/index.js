eb436cd153d271702937c186e2f23da9ba9a40dd
Mehmet <makifermis@gmail.com>
2023-12-07 23:17:51 +0100

    dice website finished

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,3 +1,5 @@
 document.querySelector("ul > li:nth-of-type(3)").innerHTML = "Mehmet"
 document.querySelector("a").style.color = "red"
 document.querySelector("button").style.backgroundColor = "yellow"
+document.querySelector("h1").classList.add("huge");
+
