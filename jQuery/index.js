dcf9bd70fb063f3a74748648ea501ac0fc42371d
Mehmet <makifermis@gmail.com>
2023-12-11 23:00:18 +0100

    jQuery learning finished

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,2 +1,17 @@
-$("h1").addClass("big-title margin-50");
+$("h1").click(function(){
+    
+})
+
+$("button").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity:0.5})
+
+    setTimeout(function(){
+        $("h1").animate({opacity: 1})
+    
+       }, 1000);
+ })
+
+$("input").keydown(function(event){
+    $("h1").text(event.key)
+})
 
