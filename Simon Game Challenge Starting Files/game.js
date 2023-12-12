3f65fe0ae93e970b6dbb6df98989de33c860013c
Mehmet <makifermis@gmail.com>
2023-12-12 23:40:08 +0100

    Simon game - step 5 ended

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,14 +1,24 @@
+var userClickedPattern= []
 var gamePattern = []
 var buttonColours = ["red", "blue", "green", "yellow"]
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4)
-    return randomNumber
+    var randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour)
+    $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+    playSound(randomChosenColour)
 }
 
-var randomChosenColour = buttonColours[nextSequence()]
+$('[type="button"]').on( "click", function() {
+    var userChosenColour = $(this).attr("id")
+    userClickedPattern.push(userChosenColour)
+    playSound(userChosenColour)
+  } );
 
-gamePattern.push(randomChosenColour)
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
 
-$('#'+randomChosenColour).fadeOut(100).fadeIn(100)
 
