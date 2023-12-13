dd924d2103c407cc69b43af961f5b24962b971da
Mehmet <makifermis@gmail.com>
2023-12-13 23:09:25 +0100

    Simon game terminated

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,14 +2,19 @@ var userClickedPattern= []
 var gamePattern = []
 var buttonColours = ["red", "blue", "green", "yellow"]
 var level = 0
+var started = false;
+ 
 
 function nextSequence(){
+    userClickedPattern = []
+    level++;
+     $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4)
     var randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
     $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
     playSound(randomChosenColour)
-    $("#level-title").text("Level "+ ++level)
+   
 }
 
 $('[type="button"]').on("click", function() {
@@ -17,13 +22,15 @@ $('[type="button"]').on("click", function() {
     userClickedPattern.push(userChosenColour)
     playSound(userChosenColour)
     animatePress(userChosenColour)
-    checkAnswer(level)
+    checkAnswer(userClickedPattern.length-1)
   } );
 
 $(document).on("keydown", function(){
-    if(level === 0){
-      nextSequence()
-    }
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+      }
     
 })
 
@@ -42,14 +49,37 @@ function animatePress(currentColour){
 }
 
 function checkAnswer(currentLevel){
-    for (let index = 0; index < currentLevel; index++) {
-        if(gamePattern[index] === userClickedPattern[index]){
-            console.log("success")            
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
-        }else{
-            console.log("failed")           
+        //console.log("success");
+  
+        //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+        if (userClickedPattern.length === gamePattern.length){
+  
+          //5. Call nextSequence() after a 1000 millisecond delay.
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+  
+        }
+  
+      } else {        
+        playSound("wrong")
+        //console.log("wrong");
+        $("body").addClass("game-over")
 
-        }       
-    }
+        setTimeout(function(){
+            $("body").removeClass("game-over")
+        }, 200)
+        $("h1").text("Game Over, Press Any Key to Restart")
+        startOver()
+  
+      }
 }
 
+function startOver(){
+    level = 0
+    gamePattern = []
+    started = false
+}
