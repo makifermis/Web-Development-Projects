32a7f546be0edda67fd5c09a798ce1a3e774f065
Mehmet <makifermis@gmail.com>
2023-12-13 14:00:39 +0100

    Update game.js

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,7 @@
 var userClickedPattern= []
 var gamePattern = []
 var buttonColours = ["red", "blue", "green", "yellow"]
+var level = 0
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4)
@@ -8,17 +9,47 @@ function nextSequence(){
     gamePattern.push(randomChosenColour)
     $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
     playSound(randomChosenColour)
+    $("#level-title").text("Level "+ ++level)
 }
 
-$('[type="button"]').on( "click", function() {
+$('[type="button"]').on("click", function() {
     var userChosenColour = $(this).attr("id")
     userClickedPattern.push(userChosenColour)
     playSound(userChosenColour)
+    animatePress(userChosenColour)
+    checkAnswer(level)
   } );
+
+$(document).on("keydown", function(){
+    if(level === 0){
+      nextSequence()
+    }
+    
+})
 
 function playSound(name){
     var audio = new Audio("sounds/"+name+".mp3");
     audio.play();
 }
 
+function animatePress(currentColour){
+   $('#'+currentColour).addClass("pressed")
+   
+   setTimeout(function(){
+    $('#'+currentColour).removeClass("pressed")
+
+   }, 100);
+}
+
+function checkAnswer(currentLevel){
+    for (let index = 0; index < currentLevel; index++) {
+        if(gamePattern[index] === userClickedPattern[index]){
+            console.log("success")            
+
+        }else{
+            console.log("failed")           
+
+        }       
+    }
+}
 
