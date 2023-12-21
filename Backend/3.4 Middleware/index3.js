5a543793c5dbd237efb9be50614711c4e6018ffd
Mehmet <makifermis@gmail.com>
2023-12-21 23:51:06 +0100

    Backend continues

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -3,7 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(logger);
+function logger(req,res,next){
+  console.log("Request method: ", req.method)
+  console.log("Request URL: ", req.url)
+  next()
+}
+
+app.use(logger)
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
