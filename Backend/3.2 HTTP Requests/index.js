38afcf24c04d434e5931ab0076f12c810b93764b
Mehmet <makifermis@gmail.com>
2023-12-21 14:06:01 +0100

    middleware started

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -4,7 +4,15 @@ const app = express()
 const port = 3000
 
 app.get("/", (req,res) =>{
-    res.send("Hello, World!")
+    res.send("<h1>Hello!</h1>")
+})
+
+app.get("/about", (req,res) =>{
+    res.send("<h1>About me!</h1>")
+})
+
+app.get("/contact", (req,res) =>{
+    res.send("<h1>Contact me!</h1>")
 })
 
 app.listen(port, () => {
